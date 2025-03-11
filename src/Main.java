f09c37a7b3f79282e2ba63bacaab6bbdb6af10ba
abhi-business <natecobusiness12@gmail.com>
2025-03-11 21:07:07 +0530

    Initial commit - Basic Project Structure

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// This is the main entry point for our Airline Booking System
+public class Main {
+    public static void main(String[] args) {
+        // Display welcome message to the user
+        System.out.println("====================================");
+        System.out.println("  Welcome to Airline Booking System  ");
+        System.out.println("====================================");
+    }
+}
